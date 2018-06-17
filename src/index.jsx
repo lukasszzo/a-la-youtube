006826765c6de359453fe1fb7b05cc1d7eb8da7a
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-06-17 19:00:28 +0200

    Environment & project setup

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const apiKey='AIzaSyAqxyVRbUTmGH_kLl01t4kZ-B85lkAGETc';
+
+ const App=() =>    {
+     return <div> HI! </div>;
+ }
+
+ReactDOM.render(<App />,document.querySelector('.container'));
